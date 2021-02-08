0a07ae6614a563ff801eb9b0a91da94605fd0786
Ryan Berwald <rberwald@ceiamerica.com>
2021-02-08 14:49:58 -0500

    Restructured files, got token grabbed and working but cannot retrieve it using content -- working on this now

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ContentComponent } from './content/content.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { FooterComponent } from './footer/footer.component';
-import { CommentsComponent } from './comments/comments.component'
-
+import { CommentsComponent } from './comments/comments.component';
 
 @NgModule({
   declarations: [
@@ -16,14 +15,15 @@ import { CommentsComponent } from './comments/comments.component'
     ContentComponent,
     NavbarComponent,
     FooterComponent,
-    CommentsComponent
+    CommentsComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    HttpClientModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
--- src/app/content/content.component.ts
+++ src/app/content/content.component.ts
@@ -1,16 +1,22 @@
-import { Component, OnInit } from '@angular/core';
-import { CommentsComponent } from '../comments/comments.component';
+import { Component, OnInit, Input } from '@angular/core';
+import { AuthService } from '../services/auth.service';
+import { Auth } from '../interfaces/auth';
 
 @Component({
   selector: 'app-content',
   templateUrl: './content.component.html',
-  styleUrls: ['./content.component.css']
+  styleUrls: ['./content.component.css'],
 })
 export class ContentComponent implements OnInit {
-
-  constructor() { }
-
+  constructor(private authService: AuthService) {}
+  auth: Auth | undefined;
   ngOnInit(): void {
+    console.log('in content');
+    //gets auth token from services/auth.service.ts -- getting token works correctly, just does not work here
+    this.authService.getToken('read').subscribe((data) => {
+      this.auth = data;
+      console.log(data); //returns undefined
+      //data contains an auth object that is supposed to get set to the auth variable, but is undefined
+    });
   }
-
 }
