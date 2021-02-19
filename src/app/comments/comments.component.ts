b26cb9bb4acfbbb92ee6f0a2217ddbbf5fe5165a
Ryan Berwald <rberwald@ceiamerica.com>
2021-02-19 17:00:39 -0500

    Refactored comments component

--- src/app/comments/comments.component.ts
+++ src/app/comments/comments.component.ts
@@ -13,8 +13,7 @@ import { Observable } from 'rxjs';
 export class CommentsComponent implements OnInit {
   @Input()
   auth!: Auth;
-  //finalComments: String[] = [];
-  commentResponse!:Observable<Comments[]>;
+  $commentResponse!:Observable<Comments[]>;
   modalRef!: BsModalRef;
   userURL = '';
   constructor(
@@ -24,29 +23,14 @@ export class CommentsComponent implements OnInit {
 
   ngOnInit(): void {}
 
-  getComments() {
-    //let commentResponse: Comments[] = [];
-
-    this.auth.scope = 'read';
-    this.commentResponse =this.commentService
-      .getComments(this.userURL, this.auth.access_token, this.auth.scope);
-      /* .subscribe((comments) => {
-        commentResponse = comments;
-        console.log(commentResponse);
-      }) */;
-    /*     for (let x = 0; x < commentResponse[0].data.children.length; x++) {
-      this.finalComments.push(commentResponse[1].data.children[x].data.body);
-    }
-    return this.finalComments; */
-    
-  }
-
   openModal(template: TemplateRef<any>) {
     this.modalRef = this.modalService.show(template);
   }
 
   onKey(value:string){
     this.userURL = value;
-    this.getComments();
+    this.auth.scope = 'read';
+    this.$commentResponse = this.commentService
+      .getComments(this.userURL, this.auth.access_token, this.auth.scope);
   }
 }
