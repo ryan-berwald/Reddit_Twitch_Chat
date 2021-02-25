0e267eff5fe24f0105b74fbf950d24e6b7b62c04
Andrew Preston <arp119@pitt.edu>
2021-02-25 09:07:22 -0500

    Refactors code so content component is no longer necessary

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from './services/auth.service';
+import { Auth } from './interfaces/auth';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  constructor(private authService: AuthService) {}
+  auth: Auth | undefined;
+  ngOnInit(): void {
+    this.authService.getToken('read').subscribe((data) => {
+      this.auth = data;
+      return data;
+    });
+  }
   title = 'Reddit-Twitch-Chat';
 }
