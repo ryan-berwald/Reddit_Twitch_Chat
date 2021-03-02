0e0210334421f7b48538ca6fecb1a41ddaed7516
Ryan Berwald <rberwald@gmail.com>
2021-03-01 21:09:20 -0500

    Adjusted to hopefully fix heroku errors #4

--- server.js
+++ server.js
@@ -1,24 +1,9 @@
-//Install express server
+// server.js
 const express = require("express");
-const path = require("path");
-
 const app = express();
-
-const forceSSL = function () {
-  return function (req, res, next) {
-    if (req.headers["x-forwarded-proto"] !== "https") {
-      return res.redirect(["https://", req.get("Host"), req.url].join(""));
-    }
-    next();
-  };
-};
-
-// Serve only the static files form the dist directory
-app.use(express.static(__dirname + "/dist/reddit-twitch-chat"));
-
-app.get("/*", function (req, res) {
-  res.sendFile(path.join(__dirname + "/dist/Reddit-Twitch-Chat/index.html"));
-});
+// Run the app by serving the static files
+// in the dist directory
+app.use(express.static(__dirname + "/dist"));
 
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
