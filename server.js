c7b7a45272df4a31804858c4bfd9c17c90907c2f
Ryan Berwald <rberwald@gmail.com>
2021-03-01 21:38:26 -0500

    Adjusted to hopefully fix heroku errors #6

--- server.js
+++ server.js
@@ -1,9 +1,12 @@
-// server.js
+function requireHTTPS(req, res, next) {
+  // The 'x-forwarded-proto' check is for Heroku
+  if (!req.secure && req.get("x-forwarded-proto") !== "https") {
+    return res.redirect("https://" + req.get("host") + req.url);
+  }
+  next();
+}
 const express = require("express");
 const app = express();
-// Run the app by serving the static files
-// in the dist directory
-app.use(express.static(__dirname + "/dist/Reddit-Twitch-Chat/index.html"));
-
-// Start the app by listening on the default Heroku port
+app.use(requireHTTPS);
+app.use(express.static("./dist/reddit-twitch-chat"));
 app.listen(process.env.PORT || 8080);
